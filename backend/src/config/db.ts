8eac53a6ba949118ee585a82d85eaef9854c42d5
Emmanuel Pesantes <emmanuelpesantes@gmail.com>
2026-06-11 11:42:07 -0500

    Nuevo cambio

--- backend/src/config/db.ts
+++ backend/src/config/db.ts
@@ -4,5 +4,6 @@ export const pool = mysql.createPool({
     host: process.env.HOST,
     user: process.env.USER,
     password: process.env.PASSWORD_DB,
-    database: process.env.DATABASE
+    database: process.env.DATABASE,
+    port: Number(process.env.PORT_DB)
 });
