3ac1d2e98de6466248877296dd768d61b74c64a9
Emmanuel Pesantes <emmanuelpesantes@gmail.com>
2026-06-11 11:49:49 -0500

    Configuracion de url frontend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -9,7 +9,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
+app.use(cors({ origin: process.env.FRONTEND_URL || 'http://localhost:5173', credentials: true }));
 app.use('/api/auth', rutasLogin);
 app.use('/', rutas);
 app.listen(PORT, () => console.log(chalk.grey(`Servidor corriendo en http://localhost:${PORT}`)));
